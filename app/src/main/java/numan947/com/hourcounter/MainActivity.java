0a73b82352f3b13fe38b47660da7e0d97c8791e1
numan947 <mahmudulhasan947@gmail.com>
2018-11-25 23:37:25 +0600

    Added functionality for adding new tasks

--- app/src/main/java/numan947/com/hourcounter/MainActivity.java
+++ app/src/main/java/numan947/com/hourcounter/MainActivity.java
@@ -2,24 +2,40 @@ package numan947.com.hourcounter;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.NumberPicker;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final String TASK_SAVE_STRING = "numan947.com.hourcounter.KEY_FOR_SAVED_TASK_LIST_IN_SHARED_PREFS";
+    private final int NUMBER_PICKER_MIN_VAL = 2;
+    private final int NUMBER_PICKER_MAX_VAL = 200;
+
+
     private ArrayList<TaskModel>tasks;
     private TaskAdapter taskAdapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         //get taskList From Preferences here
 
+        getArrayListFromPreference();
 
-        if(tasks==null){
-            tasks = new ArrayList<TaskModel>();
-            for(int i=0;i<5;i++)
-                tasks.add(new TaskModel("ASDF "+i,15,15+2*i));
-        }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         ListView taskList = (ListView)findViewById(R.id.taskList);
         taskAdapter = new TaskAdapter(this,tasks);
@@ -47,23 +57,33 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Title");
+                View customAlertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.input_dialog,null);
+                builder.setView(customAlertView);
 
-// Set up the input
-                final EditText input = new EditText(MainActivity.this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                builder.setView(input);
+                builder.setTitle("Create New Task");
 
-// Set up the buttons
+
+
+                final NumberPicker np = customAlertView.findViewById(R.id.numberPicker);
+                np.setMinValue(NUMBER_PICKER_MIN_VAL);
+                np.setMaxValue(NUMBER_PICKER_MAX_VAL);
+
+                final EditText et = customAlertView.findViewById(R.id.taskTitleInput);
+
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        String taskTitle = et.getText().toString();
+                        TaskModel newTask = new TaskModel(taskTitle,np.getValue(),np.getValue());
+                        MainActivity.this.tasks.add(newTask);
+                        MainActivity.this.taskAdapter.notifyDataSetChanged();
+                        MainActivity.this.updatePreferenceList();
+                        Snackbar.make(fab, "Saved New Task", Snackbar.LENGTH_LONG).show();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -74,18 +94,35 @@ public class MainActivity extends AppCompatActivity {
                 });
 
                 builder.show();
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
-
-
-
-
-
     }
 
 
+    private void getArrayListFromPreference(){
+        SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String jsonTasks =  appSharedPrefs.getString(this.TASK_SAVE_STRING,"");
+
+        if(jsonTasks=="") {
+            this.tasks = new ArrayList<>();
+            return;
+        }
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<TaskModel>>(){}.getType();
+        this.tasks = gson.fromJson(jsonTasks,type);
+    }
+
+    private void updatePreferenceList() {
+        SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
+
+
+        Gson gson = new Gson();
+        String jsonTasks = gson.toJson(this.tasks);
+        prefsEditor.putString(this.TASK_SAVE_STRING,jsonTasks);
+        prefsEditor.apply();
+    }
 
 
     @Override
